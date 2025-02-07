d680477b28da3674c1c11960ec0841be8eba21e7
AyushRay06 <rayayush47@gmail.com>
2025-02-07 23:42:38 +0530

    inngest function

--- src/actions/dashboard.action.ts
+++ src/actions/dashboard.action.ts
@@ -9,7 +9,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
 
-interface generateAIInsightsProps {}
 
 export const generateAIInsights = async (industry: string) => {
   const prompt = `
--- src/lib/inngest/client.ts
+++ src/lib/inngest/client.ts
@@ -4,4 +4,7 @@ import { Inngest } from "inngest"
 export const inngest = new Inngest({
   id: "tech-companion",
   name: "Tech-Companion",
+  credentials: {
+    apikey: process.env.GEMINI_API_KEY!,
+  },
 })
